bb30159edeb53799461a84414c411b3fd9abb7e0
EUNJEONGMUN <munej26@gmail.com>
2022-12-03 16:17:59 +0900

    feat: 재고를 확인하여 판매하는 기능

---
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -0,0 +1,29 @@
+package vendingmachine.domain;
+
+import java.util.Optional;
+
+public class VendingMachine {
+    private Stock stock = new Stock();
+    private MachineCoin machineCoin;
+    private int money;
+
+    public void addStock(Product product, int amount) {
+        stock.addStock(product, amount);
+    }
+
+    public void setMachineCoin(int money) {
+        this.machineCoin = new MachineCoin(money);
+    }
+
+    public void insertMoney(int money) {
+        this.money = money;
+    }
+
+    public void sellProduct(String productName) {
+        Optional<Product> product = stock.findByName(productName);
+        if (product.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 해당 상품이 존재하지 않습니다.");
+        }
+        stock.minusStock(product.get());
+    }
+}
