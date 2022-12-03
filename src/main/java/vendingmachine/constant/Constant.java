964abb29021db203bdeacf385164081460b3898c
EUNJEONGMUN <munej26@gmail.com>
2022-12-03 15:46:28 +0900

    refactor: 상수 이동

---
+++ src/main/java/vendingmachine/constant/Constant.java
@@ -0,0 +1,11 @@
+package vendingmachine.constant;
+
+public class Constant {
+    public static final String PRODUCT_DELIMITER = ";";
+    public static final String PRODUCT_INFO_DELIMITER = ",";
+    public static final String PRODUCT_INFO_PREFIX = "[";
+    public static final String PRODUCT_INFO_SUFFIX = "]";
+    public static final int PRODUCT_PRICE_INDEX = 1;
+    public static final int PRODUCT_AMOUNT_INDEX = 2;
+    public static final int PRODUCT_INFO_SIZE = 3;
+}
--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -1,20 +1,13 @@
 package vendingmachine.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import vendingmachine.constant.Constant;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class InputView {
-    private static final String PRODUCT_DELIMITER = ";";
-    private static final String PRODUCT_INFO_DELIMITER = ",";
-    private static final String PRODUCT_INFO_PREFIX = "[";
-    private static final String PRODUCT_INFO_SUFFIX = "]";
-    private static final int PRODUCT_PRICE_INDEX = 1;
-    private static final int PRODUCT_AMOUNT_INDEX = 2;
-    private static final int PRODUCT_INFO_SIZE = 3;
-
 
     public int readVendingMachineMoney() {
         String money = Console.readLine();
@@ -32,9 +25,10 @@ public class InputView {
 
     public List<String> readProductInfo() {
         String input = Console.readLine();
-        Arrays.stream(input.split(PRODUCT_DELIMITER))
+        Arrays.stream(input.split(Constant.PRODUCT_DELIMITER))
                 .forEach(this::validProductInfoShape);
-        return Arrays.stream(input.split(PRODUCT_DELIMITER)).collect(Collectors.toList());
+        return Arrays.stream(input.split(Constant.PRODUCT_DELIMITER))
+                .collect(Collectors.toList());
     }
 
     private void validProductInfoShape(String info) {
@@ -44,22 +38,24 @@ public class InputView {
     }
 
     private void validPrefixAndSuffix(String info) {
-        if (!(info.startsWith(PRODUCT_INFO_PREFIX) && info.endsWith(PRODUCT_INFO_SUFFIX))) {
+        if (!(info.startsWith(Constant.PRODUCT_INFO_PREFIX)
+                && info.endsWith(Constant.PRODUCT_INFO_SUFFIX))) {
             throw new IllegalArgumentException("[ERROR] 개별 상품은 대괄호로 구분해야 합니다.");
         }
     }
 
     private void validDelimiter(String info) {
-        if (info.split(PRODUCT_INFO_DELIMITER).length != PRODUCT_INFO_SIZE) {
-            throw new IllegalArgumentException("[ERROR] 상품은 상품명, 가격, 수량은 쉼표로 구분하여 입력해야 합니다.");
+        if (info.split(Constant.PRODUCT_INFO_DELIMITER).length != Constant.PRODUCT_INFO_SIZE) {
+            throw new IllegalArgumentException(
+                    "[ERROR] 상품은 상품명, 가격, 수량은 쉼표로 구분하여 입력해야 합니다.");
         }
     }
 
     private void validProductInfoValueType(String info) {
-        String[] split = info.split(PRODUCT_INFO_DELIMITER);
+        String[] split = info.split(Constant.PRODUCT_INFO_DELIMITER);
         try {
-            validIsNumber(split[PRODUCT_PRICE_INDEX]);
-            validIsNumber(split[PRODUCT_AMOUNT_INDEX]);
+            validIsNumber(split[Constant.PRODUCT_PRICE_INDEX]);
+            validIsNumber(split[Constant.PRODUCT_AMOUNT_INDEX]);
         } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException("[ERROR] 가격과 수량은 숫자로 입력해야 합니다.");
         }
