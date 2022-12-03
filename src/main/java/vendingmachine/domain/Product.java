dcacf8397f5454d1e45e4d257b88550237f4ac75
EUNJEONGMUN <munej26@gmail.com>
2022-12-03 12:57:23 +0900

    feat: 상품객체 생성

---
+++ src/main/java/vendingmachine/domain/Product.java
@@ -0,0 +1,55 @@
+package vendingmachine.domain;
+
+import java.util.Objects;
+
+public class Product {
+    private static final int PRICE_MINIMUM = 100;
+    private static final int PRICE_UNIT = 10;
+
+    private final String name;
+    private final int price;
+
+    private Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Product of(String name, int price) {
+        validPrice(price);
+        return new Product(name, price);
+    }
+
+    private void validPrice(int price) {
+        validOverMinimumPrice(price);
+        validPriceUnit(price);
+    }
+
+    private void validOverMinimumPrice(int price) {
+        if (price < PRICE_MINIMUM) {
+            throw new IllegalArgumentException("[ERROR] 상품가격은 100원부터 시작합니다.");
+        }
+    }
+
+    private void validPriceUnit(int price) {
+        if (price % PRICE_UNIT != 0) {
+            throw new IllegalArgumentException("[ERROR] 상품가격은 10원으로 나누어떨어져야 합니다.");
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Product)) {
+            return false;
+        }
+        if (((Product) obj).name.equals(this.name)
+                && ((Product) obj).price == this.price) {
+            return true;
+        }
+        return false;
+    }
+}
