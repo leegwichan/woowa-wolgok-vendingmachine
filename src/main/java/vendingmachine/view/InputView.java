02c93abfe0adc7411fa2bcfd27f504c863038891
EUNJEONGMUN <munej26@gmail.com>
2022-12-03 16:43:26 +0900

    feat: 입력 안내 문구 출력

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 public class InputView {
 
     public int readVendingMachineMoney() {
+        System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
         String money = Console.readLine();
         validIsNumber(money);
         return Integer.parseInt(Console.readLine());
@@ -24,6 +25,7 @@ public class InputView {
     }
 
     public List<String> readProductInfo() {
+        System.out.println("상품명과 가격, 수량을 입력해 주세요.");
         String input = Console.readLine();
         Arrays.stream(input.split(Constant.PRODUCT_DELIMITER))
                 .forEach(this::validProductInfoShape);
@@ -62,12 +64,14 @@ public class InputView {
     }
 
     public int readMoney() {
+        System.out.println("투입 금액을 입력해 주세요.");
         String money = Console.readLine();
         validIsNumber(money);
         return Integer.parseInt(money);
     }
 
     public String readProductName() {
+        System.out.println("구매할 상품명을 입력해 주세요.");
         return Console.readLine();
     }
 }
