47982865e07ca66c6275bdb7dc9cb07ab1b30025
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2023-01-03 21:33:36 +0900

    refactor(OutputView) : print method 분리
    - 추후 print 방식 변경을 용이하게 하기 위해 다른 method에도 사용될 print 메서드 구현
    - printErrorMessage() 내부에서 print()를 사용하도록 함

--- src/main/java/vendingmachine/view/output/OutputView.java
+++ src/main/java/vendingmachine/view/output/OutputView.java
@@ -5,6 +5,8 @@ import java.util.LinkedHashMap;
 
 public class OutputView {
 
+    private final String INPUT_AMOUNT = "투입 금액 : ";
+
     public static void printHoldingCoins(LinkedHashMap<Coin, Integer> holdingCoins) {
 
     }
@@ -18,6 +20,10 @@ public class OutputView {
     }
 
     public static void printErrorMessage(Exception exception) {
-        System.out.println(exception.getMessage());
+        print(exception.getMessage());
+    }
+
+    private static void print(String message) {
+        System.out.println(message);
     }
 }
